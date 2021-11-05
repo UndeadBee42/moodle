d96cfad82bf0cc6d82c18333b352c25a67efec02
bushman16 <bushmanov16@mail.ru>
2021-11-05 16:53:30 +0300

    1234

--- moodle/db_interact.cs
+++ moodle/db_interact.cs
@@ -11,7 +11,7 @@ namespace db_interact
         void connect();
         Dictionary<string, string> get_settings();
         void set_settings(Dictionary<string, string> s);
-        // добавить пользователей
+        // добавить пользователей(студентов)
     }
     public class sql_server : db_interact
     {
